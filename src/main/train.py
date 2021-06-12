4828b322d9c041572e06a3f0fc5f6886560b53c4
giannikiss <steliosgiannik@gmail.com>
2021-06-12 02:22:03 +0100

    Add command line training arguments

--- src/main/train.py
+++ src/main/train.py
@@ -22,10 +22,13 @@ def run_training(opts) -> None:
     pipe.fit(X_train)
     proc_data_x = pipe.transform(X_train)
     proc_data_test = pipe.transform(X_test)
-    
+
     # Initialize and fit the model
-    model = LSTMModel()
-    model.fit(proc_data_x, y_train, epochs=2, validation_data=(proc_data_test, y_test))
+    model = LSTMModel(lstm_units=opts.lstm_units,
+                      embedding_dim=opts.embedding_dim,
+                      input_dim=opts.input_dim,
+                      optimizer=opts.optimizer)
+    model.fit(proc_data_x, y_train, epochs=opts.epochs, validation_data=(proc_data_test, y_test))
     
     params = model.get_numeric_params
     avg_metrics = {}
@@ -48,5 +51,10 @@ if __name__ == "__main__":
     parser.add_argument("--source", required=True, type=str, help="Source path of the training dataset")
     parser.add_argument("--pipe_path", required=True, type=str, help="Path to save the pipeline")
     parser.add_argument("--model_path", required=True, type=str, help="Path to save the trained model")
+    parser.add_argument("--lstm_units", required=False, default=15, type=int, help="Number of units in a layer")
+    parser.add_argument("--embedding_dim", required=False, default=20, type=int, help="Embedding dimension")
+    parser.add_argument("--input_dim", required=False, default=7295, type=int, help="Input dimension")
+    parser.add_argument("--optimizer", required=False, default='adam', type=str, help="Model optimizer")
+    parser.add_argument("--epochs", required=False, default=2, type=int, help="Number of epochs")
     args = parser.parse_args()
     run_training(args)
