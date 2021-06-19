3273cf5bddc7d81f51c56da34f1c0771616d72e9
giannikiss <steliosgiannik@gmail.com>
2021-06-19 01:22:57 +0100

    Put model loading outside of GET

--- src/main/API.py
+++ src/main/API.py
@@ -1,11 +1,16 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 from predict import run_prediction
+from preprocess.data_manager import load_pipeline
+from tensorflow.keras.models import load_model
 
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates/")
 
+# Load model and pipeline
+pipe = load_pipeline(path='trained_pipe/', file_name='pipe.pkl')
+model = load_model('trained_models/')
 
 @app.get('/')
 def read_root():
@@ -19,6 +24,6 @@ def form_post(request: Request):
 
 @app.post("/predict")
 def form_post(request: Request, word: str = Form(...)):
-    result = run_prediction(word)
+    result = run_prediction(word, model, pipe)
     return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
     
--- src/main/predict.py
+++ src/main/predict.py
@@ -1,17 +1,11 @@
 import pandas as pd
 import numpy as np
-from preprocess.data_manager import load_pipeline
-from tensorflow.keras.models import load_model
 
 
 
-def run_prediction(word):
+def run_prediction(word, model, pipe):
     """Generate predictions"""
     data = pd.Series(word)
-    
-    # Load pipeline and model
-    pipe = load_pipeline(path='trained_pipe/', file_name='pipe.pkl')
-    model = load_model('trained_models/')
 
     # Transform the data
     proc_data = pipe.transform(data)
